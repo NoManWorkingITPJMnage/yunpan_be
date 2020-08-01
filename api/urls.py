72a1b15286164042ee11d2f3f6738f57564aab82
StarashZ <15773541150>
2020-08-02 01:59:16 +0800

    backend code

--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     path('signin', views.login),
-    path('signup', views.logout),
-    path('register', views.register),
+    path('signout', views.logout),
+    path('signup', views.register),
     path('user', views.content),
     path('folders', views.folders),
     path('resources', views.resources),
     path('folder', views.folder),
-    path('folder/download', views.folder_download)
+    path('folder/download', views.folder_download),
 ]
